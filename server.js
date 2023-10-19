12eeae2821e691d82d0dba1dc26340ce4b85073d
Haroon Frotan <haroon_fro@hotmail.com>
2023-10-19 10:40:18 -0400

    server good

--- server.js
+++ server.js
@@ -33,7 +33,19 @@ app.post('/multi', multiController.createMulti)
 app.put('/multi/:id', multiController.updateMulti)
 app.delete('/multi/:id', multiController.deleteMulti)
 
+app.get('/creatine', creatineController.getAllCreatine)
+app.get('/creatine/:id', creatineController.getCreatineById)
+app.post('/creatine', creatineController.createCreatine)
+app.put('/creatine/:id', creatineController.updateCreatine)
+app.delete('/creatine/:id', creatineController.deleteCreatine)
+
+app.get('/stim', stimController.getAllStim)
+app.get('/stim/:id', stimController.getStimById)
+app.post('/stim', stimController.createStim)
+app.put('/stim/:id', stimController.updateStim)
+app.delete('/stim/:id', stimController.deleteStim)
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
     })
+
