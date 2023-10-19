b2449977aace1223412f764e1a90c4beba3d671e
Haroon Frotan <haroon_fro@hotmail.com>
2023-10-19 10:33:10 -0400

    updated port

--- server.js
+++ server.js
@@ -20,3 +20,20 @@ app.use(logger('dev'));
 app.get('/', (req, res) => {
     res.send('Server running on port 3001. Connected to mongoDB')
 })
+
+app.get('/protein', proteinController.getAllProtein)
+app.get('/protein/:id', proteinController.getProteinById)
+app.post('/protein', proteinController.createProtein)
+app.put('/protein/:id', proteinController.updateProtein)
+app.delete('/protein/:id', proteinController.deleteProtein)
+
+app.get('/multi', multiController.getAllMulti)
+app.get('/multi/:id', multiController.getMultiById)
+app.post('/multi', multiController.createMulti)
+app.put('/multi/:id', multiController.updateMulti)
+app.delete('/multi/:id', multiController.deleteMulti)
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+    })
